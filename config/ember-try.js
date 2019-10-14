82d9005ab99e1f5b2fd72ae7680e63dc7814f8d5
saintsebastian <elvina.valieva@people-doc.com>
2019-10-14 10:23:43 +0200

    chode: aff yarn support and update eslint-config

--- config/ember-try.js
+++ config/ember-try.js
@@ -4,6 +4,7 @@ const getChannelURL = require('ember-source-channel-url')
 
 module.exports = async function() {
   return {
+    useYarn: true,
     scenarios: [
       {
         name: 'ember-lts-3.4',
@@ -69,5 +70,5 @@ module.exports = async function() {
         }
       }
     ]
-  };
-};
+  }
+}
