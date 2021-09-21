8e4fa81e2f7f82ab2f9520fb0692b3a5889e2d94
MrChocolatine <47531779+MrChocolatine@users.noreply.github.com>
2021-09-21 11:36:06 +0100

    ci: stop testing for Ember.js beta

--- config/ember-try.js
+++ config/ember-try.js
@@ -31,14 +31,6 @@ module.exports = async function () {
         },
       },
       {
-        name: 'ember-beta',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('beta'),
-          },
-        },
-      },
-      {
         name: 'ember-canary',
         npm: {
           devDependencies: {
